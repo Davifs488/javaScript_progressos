70e947e48c93db028491f35e61843027dec2bb86
Davi Ferreira da Silva <sildv45@gmail.com>
2023-07-17 14:55:32 -0300

    Estudos,projetos e evoluções

---
+++ casting_tipos.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+   <head>
+	    <meta charset="utf-8">
+    	<title>Curso PHP</title>
+    </head>
+
+    <body>
+    	  <?php
+
+          //gettype() => retorna o tipo da variável
+          $valor = 10;
+
+          echo gettype($valor);
+
+
+         
+        ?>
+     
+    </body>
+    </html>
